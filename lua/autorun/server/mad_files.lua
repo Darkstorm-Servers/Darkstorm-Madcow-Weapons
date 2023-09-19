d89fcb3c01f5d852eae9888af29bbbab81930956
Bryant <bryantdl7@hotmail.com>
2023-09-19 18:30:14 -0400

    remove the forced workshop downloads

--- lua/autorun/server/mad_files.lua
+++ lua/autorun/server/mad_files.lua
@@ -6,11 +6,11 @@
     mmm       mmm aaa     aaa dddddddddd	  File: mad_files.lua
 ---mmm--------mmm-aaa-----aaa-ddddddddd---------------------------------------->
 ---------------------------------------------------------*/
-resource.AddWorkshop(147708337) 
-resource.AddWorkshop(165696777) 
-//resource.AddWorkshop(169011381)
-//resource.AddWorkshop(172958536)
-//resource.AddWorkshop(136509434)
-//resource.AddWorkshop(105067766)
-resource.AddWorkshop(173482196)
-resource.AddWorkshop(104498895)
+--resource.AddWorkshop(147708337) 
+--resource.AddWorkshop(165696777) 
+--//resource.AddWorkshop(169011381)
+--//resource.AddWorkshop(172958536)
+--//resource.AddWorkshop(136509434)
+--//resource.AddWorkshop(105067766)
+--resource.AddWorkshop(173482196)
+--resource.AddWorkshop(104498895)
